5654af8305bff1c8fbca3a90249dce2fc1086069
andela-oowonikoko <oluwaseun.owonikoko@andela.com>
2017-03-23 17:55:40 +0100

    chore #142327099 (modify): modify file (modify file server.js to rectify hound flags)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+
+const app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -10,13 +11,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
 // set the home page route
-app.get('/', function(req, res) {
-  res.send({message: "welcome to document management system"});
+app.get('/', (req, res) => {
+  res.send({ message: 'welcome to document management system' });
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`Our app is running on http://localhost:${port}`);
 });
