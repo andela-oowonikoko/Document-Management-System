b86786c0ff67155ad5d4bc4034c51d9843b52a3a
andela-oowonikoko <oluwaseun.owonikoko@andela.com>
2017-03-25 18:06:07 +0100

    chore 142306877 (modify): modify file (modify server/config/config.js to es6 standard

--- server/config/config.js
+++ server/config/config.js
@@ -1,26 +1,26 @@
-{
-  "development": {
-    "username": "koko",
-    "password": "owonikoko",
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "port": 5432,
-    "dialect": "postgres"
+module.exports = {
+  development: {
+    username: 'koko',
+    password: 'owonikoko',
+    database: 'database_development',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
   },
-  "test": {
-    "username": "koko",
-    "password": "owonikoko",
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "port": 5432,
-    "dialect": "postgres"
+  test: {
+    username: 'koko',
+    password: 'owonikoko',
+    database: 'database_test',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
   },
-  "production": {
-    "username": "koko",
-    "password": "owonikoko",
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "port": 5432,
-    "dialect": "postgres"
+  production: {
+    username: 'koko',
+    password: 'owonikoko',
+    database: 'database_production',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
   }
-}
+};
