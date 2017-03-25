d3291b2b27021ddac2a8cac528da136aca5eeb08
andela-oowonikoko <oluwaseun.owonikoko@andela.com>
2017-03-25 15:43:31 +0100

    chore 142306877 add: add files (files added after sequelize was initialized)

---
+++ server/config/config.js
@@ -0,0 +1,26 @@
+{
+  "development": {
+    "username": "koko",
+    "password": "owonikoko",
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "port": 5432,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "koko",
+    "password": "owonikoko",
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "port": 5432,
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "koko",
+    "password": "owonikoko",
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "port": 5432,
+    "dialect": "postgres"
+  }
+}
