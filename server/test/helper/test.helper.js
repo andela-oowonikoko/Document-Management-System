49c566c947f167f659f33acdadf1f3eaf296c1ff
andela-oowonikoko <oluwaseun.owonikoko@andela.com>
2017-03-31 15:56:08 +0100

    chore 142585975 (add): add file (add server/test/models/role.spec.js file and added tests for the roles model modify the server/test/models/user.spec.js to add more tests for the user model

--- server/test/helper/test.helper.js
+++ server/test/helper/test.helper.js
@@ -1,6 +1,28 @@
 import faker from 'faker';
 
 const helper = {
+  adminRole: {
+    id: 1,
+    title: 'admin'
+  },
+  regularRole: {
+    id: 2,
+    title: 'regular'
+  },
+  firstUser: {
+    username: faker.internet.userName(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  },
+  secondUser: {
+    username: faker.internet.userName(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  },
   regularUser: {
     username: faker.internet.userName(),
     firstName: faker.name.firstName(),
@@ -14,6 +36,13 @@ const helper = {
     lastName: faker.name.lastName(),
     email: 'invalidemail',
     password: faker.internet.password(),
+  },
+  invalidPasswordUser: {
+    username: faker.internet.userName(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: 'koko'
   }
 };
 export default helper;
